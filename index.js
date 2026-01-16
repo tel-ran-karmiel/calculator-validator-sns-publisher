2d824d26342434c48e76a6d0101b51853d339a44
Yuragranovsky <yuragranovsky@gmail.com>
2026-01-16 18:20:18 +0200

    HW #14 Solution

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-const zod = require("zod")
+const zod = require("zod");
+const pino = require("pino");
+const logger = pino({
+  level: process.env.LOGGER_LEVEL || "info",
+  base: undefined,
+  timestamp: false,
+  formatters: {
+    level: (label) => {
+      return { level: label.toUpperCase() };
+    },
+  }
+});
+logger.info(`debug level is ${logger.level}`)
 function handler(event) {
-   const CalcSechema = zod.object({
-    op1:zod.number(),
-    op2:zod.number(),
-    operation: zod.string()
-   })
-   try {
-    const data = CalcSechema.parse(JSON.parse(event.body))
-    return response(200, event.body)
-   } catch (error) {
-      return response (400, error.message)
-   }
-  
+  const CalcSechema = zod.object({
+    op1: zod.number(),
+    op2: zod.number(),
+    operation: zod.string(),
+  });
+  logger.debug(`recieved data are ${event.body}` )
+  try {
+   CalcSechema.parse(JSON.parse(event.body));
+   logger.debug('after JSON parsing: valid data')
+    return response(200, event.body);
+  } catch (error) {
+    logger.error(`validation error: ${error.message}`)
+    return response(400, error.message);
+  }
 }
-handler()
-function response(code, body){
-    return {
-        "status": code,
-        "body": body
-    }
+
+function response(code, body) {
+  return {
+    statusCode: code,
+    body: body,
+  };
 }
 module.exports = {
-    handler
-}
+  handler,
+};
